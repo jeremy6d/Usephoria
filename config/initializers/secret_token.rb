ef630af1a3bfd3b74c4b13572ef3b96ed4aeabd1
Willfire19 <willfire19@gmail.com>
2013-07-27 10:43:09 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Usephoria::Application.config.secret_token = '5299034a6f4b8826d7c15c9a6e1cae36176861cb5c152e20ea7d42670fa6c7672ce2503c8d226fcbc6bd3c7373427fef4344dfbfce3c8bd8789c70fd668d6858'

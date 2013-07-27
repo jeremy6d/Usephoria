bf2a469b41d287c3fe5b3e0caf3e200a236e890b
tiffylou <tiffany6872@gmail.com>
2013-07-27 18:00:35 -0400

    TF: Homepage setup in Ruby, CSS Path

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,14 @@ Usephoria::Application.routes.draw do
   
   resources :test_definitions
 
-  devise_for :users do 
-    get "/", :to => "devise/registrations#new"
-  end
+  devise_for :users #do 
+    #get "/", :to => "devise/registrations#new"
+  #end
 
   get "static/dashboard"
+  get "static/landing"
+  root :to => "static#landing"
 
-  root :to => "devise/registrations#new"
+
+  #root :to => "devise/registrations#new"
 end

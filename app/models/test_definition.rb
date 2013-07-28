5433d328b2dad8cc6d877d67780b8c1bb79e29cf
tiffylou <tiffany6872@gmail.com>
2013-07-28 09:59:44 -0400

    integrated views for testers/index

--- app/models/test_definition.rb
+++ app/models/test_definition.rb
@@ -5,6 +5,7 @@ class TestDefinition
   field :title, type: String
   field :active, type: Boolean, default: true
   field :user_goal, type: Integer, default: 0
+  field :price, type: Integer, default: 1
 
   # belongs_to :author, class_name: "User"
   has_many :results, class_name: "TestResult"

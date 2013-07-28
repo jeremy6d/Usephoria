6fe2551a36afe5c836f76a0748d9dfb1978e305c
Giuseppe Mancone <manconeg@gmail.com>
2013-07-28 14:27:54 -0400

    Login redirects to correct dashboards

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -10,6 +10,14 @@ class StaticController < ApplicationController
   def howItWorks
   end
   def landing
+    if(user = current_user())
+      if(user.role == 'client')
+        redirect_to clients_root_path
+      else
+        redirect_to testers_root_path
+      end
+      return
+    end
 	  render :layout => false
   end
 end

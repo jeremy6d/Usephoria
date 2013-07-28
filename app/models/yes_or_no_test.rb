aac9b770cb23a4198d36512134ef68cbd8e48fe3
Willfire19 <willfire19@gmail.com>
2013-07-28 10:44:34 -0400

    Some changes on yn tests

--- app/models/yes_or_no_test.rb
+++ app/models/yes_or_no_test.rb
@@ -1,5 +1,15 @@
 class YesOrNoTest < TestDefinition
-  field :question, type: String
+  field :question
 
+  mount_uploader :image, ImageUploader
 
+  attr_accessible :image,
+				  				:image_cache,
+				  				:question,
+				  				:title
+
+	def aggregate_result
+		yes_count = results.where(:answer => "Yes").count
+		no_count = results.where(:answer => "No").count
+	end
 end
